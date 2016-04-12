84a7b8013889514125e9aaf8984c7bdd78fa206c
Camilohd <camher0589@gmail.com>
2016-04-12 15:46:32 -0500

    subiendo actualizacion paginas y listado usuario

--- codigo/proagro/src/main/webapp/pages/login/login.js
+++
@@ -1,7 +0,0 @@
-angular.module('Login').controller('LoginController', function($scope) {
-    $scope.email = "John";
-    $scope.password = "Doe";
-    $scope.calcularNombre =function(){
-    	$scope.fullName = $scope.email+ " " + $scope.password;
-    };
-});
--- codigo/proagro/src/main/webapp/pages/usuario/usuario.js
+++ codigo/proagro/src/main/webapp/pages/usuario/usuario.js
@@ -1,120 +1,190 @@
-angular.module('Usuario')
-.service('UsuarioService',  function($http) {
+angular.module('Usuario').service('UsuarioService', function($http) {
 
-   
-   this.crearusuario = function(data,callback,callerror){
-        $http(
-          { method: 'POST',
-            url: 'http://localhost:8082/proagro/rest/usuario/crearUsuario',
-            data: data }).
-          success(function (response) {
-                callback(response);
-                console.log("sucessfull "+JSON.stringify(response));
-          }).
-          error(function () {
-            callerror();
-            console.log("error");
-          });
-      };
+	this.crearusuario = function(data, callback, callerror) {
+		$http({
+			method : 'POST',
+			url : 'http://localhost:8082/proagro/rest/usuario/crearUsuario',
+			data : data
+		}).success(function(response) {
+			callback(response);
+			console.log("sucessfull " + JSON.stringify(response));
+		}).error(function() {
+			callerror();
+			console.log("error");
+		});
+	};
 
-      this.editarUsuario = function(data,callback,callerror){
-        $http(
-          { method: 'POST',
-            url: 'http://localhost:8082/proagro/rest/usuario/editarUsuario',
-            data: data }).
-          success(function (response) {
-                callback(response);
-                console.log("sucessfull "+JSON.stringify(response));
-          }).
-          error(function () {
-            callerror();
-            console.log("error");
-          });
-      };
+	this.login = function(data, callback, callerror) {
+		$http({
+			method : 'POST',
+			url : 'http://localhost:8082/proagro/rest/usuario/login',
+			data : data,
+			headers : {
+				'Content-Type' : 'application/x-www-form-urlencoded'
+			}
+		}).success(function(response) {
+			callback(response);
+		}).error(function() {
+			callerror();
+		});
+	};
 
-       this.obtenerUsuario = function(data,callback,callerror){
-        $http(
-          { method: 'POST',
-            url: 'http://localhost:8082/proagro/rest/usuario/obtenerUsuario',
-            data: data }).
-          success(function (response) {
-                callback(response);
-                console.log("sucessfull "+JSON.stringify(response));
-          }).
-          error(function () {
-            callerror();
-            console.log("error");
-          });
-      };
-    }
-  
-)
-.controller('UsuarioController', function($scope,$location, UsuarioService,MessageService,$routeParams) {
-    $scope.user="";
+	this.logout = function(data, callback, callerror) {
+		$http({
+			method : 'POST',
+			url : 'http://localhost:8082/proagro/rest/usuario/logout',
+			data : data,
+			headers : {
+				'Content-Type' : 'application/x-www-form-urlencoded'
+			}
+		}).success(function(response) {
+			callback(response);
+		}).error(function() {
+			callerror();
+		});
+	};
 
-    $scope.inicializar = function(){
-      $scope.id = $routeParams.id;
-      $scope.editando=false;
-      if($scope.id == "new"){
-        $scope.user="";
-      } else {
-          $scope.editando=true;
-          UsuarioService.obtenerUsuario($scope.id,
-            function(response){
-              if(response.code == 'OK'){
-                 $scope.user=response.object;
+	this.editarUsuario = function(data, callback, callerror) {
+		$http({
+			method : 'POST',
+			url : 'http://localhost:8082/proagro/rest/usuario/editarUsuario',
+			data : data
+		}).success(function(response) {
+			callback(response);
+			console.log("sucessfull " + JSON.stringify(response));
+		}).error(function() {
+			callerror();
+			console.log("error");
+		});
+	};
 
-              }else {
-                MessageService.setMessages(response.mensaje);
-              }
-            },function(){
-              $scope.desplegarError();
-            });
-      }
-      
-      console.log(id);
-    };
+	this.obtenerUsuario = function(data, callback, callerror) {
+		$http({
+			method : 'POST',
+			url : 'http://localhost:8082/proagro/rest/usuario/obtenerUsuario',
+			data : data
+		}).success(function(response) {
+			callback(response);
+			console.log("sucessfull " + JSON.stringify(response));
+		}).error(function() {
+			callerror();
+			console.log("error");
+		});
+	};
 
-    $scope.realizarOperacion = function(){
-      if($scope.editando){
-        $scope.editarUsuario();
-      } else {
-        $scope.crearusuario();
-      }
-    };
-    $scope.crearusuario =function(){
-      console.log(JSON.stringify($scope.user));
-      UsuarioService.crearusuario(JSON.stringify($scope.user),function(response){
-        $scope.validarRespuesta(response);
-        $scope.user="";
-        $location.path("login");
-      },function(){
-        $scope.desplegarError();
-      });
-    };
+	this.listarUsuarios = function(callback, callerror) {
+		$http({
+			method : 'POST',
+			url : 'http://localhost:8082/proagro/rest/usuario/listarUsuarios'
+		}).success(function(response) {
+			callback(response);
+		}).error(function() {
+			callerror();
+		});
+	};
+}
 
+).controller(
+		'UsuarioController',
+		function($scope, $location, UsuarioService, MessageService,
+				$routeParams) {
+			$scope.user = "";
+			$scope.usuario = {
+				email : '',
+				password : ''
+			}
 
-    $scope.editarUsuario =function(){
-      console.log(JSON.stringify($scope.user));
-        UsuarioService.editarUsuario(JSON.stringify($scope.user),function(response){
-        $scope.validarRespuesta(response);
-        $scope.user="";
-      },function(){
-        $scope.desplegarError();
-      });
-    };
+			$scope.inicializar = function() {
+				$scope.id = $routeParams.id;
+				$scope.editando = false;
+				if ($scope.id == "new") {
+					$scope.user = "";
+				} else {
+					$scope.editando = true;
+					UsuarioService.obtenerUsuario($scope.id,
+							function(response) {
+								if (response.code == 200) {
+									$scope.user = response.object;
 
-    $scope.validarRespuesta = function (response){
-      console.log(JSON.stringify(response));
-      if(response.code === 'OK'){
-        MessageService.setMessages(response.mensaje);
-      }else {
-        MessageService.setMessages(response.mensaje+" Code: "+response.code);
-      }
-    }
+								} else {
+									MessageService
+											.setMessages(response.mensaje);
+								}
+							}, function() {
+								$scope.desplegarError();
+							});
+				}
 
-    $scope.desplegarError = function (){
-        MessageService.setMessages("Error al presentar la operación.");
-  
-    }
-});
+			};
+
+			$scope.inicializarList = function() {
+				UsuarioService.listarUsuarios(function(response) {
+
+					$scope.usuarios = response;
+
+				}, function() {
+					$scope.desplegarError();
+				});
+			};
+
+			$scope.realizarOperacion = function() {
+				if ($scope.editando) {
+					$scope.editarUsuario();
+				} else {
+					$scope.crearusuario();
+				}
+			};
+			$scope.crearusuario = function() {
+				console.log(JSON.stringify($scope.user));
+				UsuarioService.crearusuario(JSON.stringify($scope.user),
+						function(response) {
+							$scope.validarRespuesta(response);
+							$scope.user = "";
+							$location.path("login");
+						}, function() {
+							$scope.desplegarError();
+						});
+			};
+
+			$scope.editarUsuario = function() {
+				console.log(JSON.stringify($scope.user));
+				UsuarioService.editarUsuario(JSON.stringify($scope.user),
+						function(response) {
+							$scope.validarRespuesta(response);
+							$scope.user = "";
+						}, function() {
+							$scope.desplegarError();
+						});
+			};
+			
+			$scope.enviarEditar = function(identificador) {
+				$location.path("/usuario/"+identificador);
+			};
+
+			$scope.validarRespuesta = function(response) {
+				console.log(JSON.stringify(response));
+				if (response.code === 'OK') {
+					MessageService.setMessages(response.mensaje);
+				} else {
+					MessageService.setMessages(response.mensaje + " Code: "
+							+ response.code);
+				}
+			};
+
+			$scope.login = function() {
+				console.log($scope.usuario);
+				var usurios = "email=" + $scope.usuario.email + "&password="
+						+ $scope.usuario.password;
+				UsuarioService.login(usurios, function(response) {
+					$scope.validarRespuesta(response);
+					if (response.code == 200) {
+						localStorage.isLogin = $scope.usuario.email;
+						$scope.login = {};
+						$location.path("inicio");
+					}
+				}, function() {
+					$scope.desplegarError();
+				});
+			};
+
+		});

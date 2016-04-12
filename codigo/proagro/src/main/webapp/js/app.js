cc6f7ff2533357057c686d11f716a07ae8c503a5
carmenhoyos9 thor <carmenhoyos9@gmail.com>
2016-04-12 15:40:55 -0500

    subiendo script

--- codigo/proagro/src/main/webapp/js/app.js
+++ codigo/proagro/src/main/webapp/js/app.js
@@ -16,13 +16,13 @@
  */
 // Define top level routes for the app, security related views are declated in the security.js
 // Note that this app is a single page app, and each partial is routed to using the URL fragment. For example, to select the 'home' route, the URL is http://localhost:8080/Project/#/home
-angular.module('Login',[]);
-angular.module('Usuario',[]);
-var appModule = angular.module('Proagro', [ 'Login','Usuario','ngRoute','ui.bootstrap' ]).config(
+angular.module('Usuario', []);
+var appModule = angular.module('Proagro',
+		[ 'Usuario', 'ngRoute', 'ui.bootstrap' ]).config(
 		[ '$routeProvider', function($routeProvider) {
 			$routeProvider.when('/login', {
-				templateUrl : 'pages/login/login.html',
-				controller : 'LoginController'
+				templateUrl : 'pages/usuario/login.html',
+				controller : 'UsuarioController'
 			// Add a default route
 			}).when('/about', {
 				templateUrl : 'pages/about/about.html',
@@ -40,63 +40,103 @@ var appModule = angular.module('Proagro', [ 'Login','Usuario','ngRoute','ui.boot
 				templateUrl : 'pages/usuario/usuario.html',
 				controller : 'UsuarioController'
 			// Add a default route
+			}).when('/usuarios', {
+				templateUrl : 'pages/usuario/listarUsuarios.html',
+				controller : 'UsuarioController'
+			// Add a default route
+			}).when('/mapa', {
+				templateUrl : 'pages/mapa/mapa.html',
+				controller : 'appctrl'
+			// Add a default route
 			}).otherwise({
 				redirectTo : '/inicio'
 			});
-		} ])
-.run(function($rootScope){
-	$rootScope.$on( "$routeChangeStart", function(event, next, current) {
-		console.log(JSON.stringify(next));
-      if (next.originalPath === '/inicio') {
-        $rootScope.showBanner=true;
-      }else {
-      	 $rootScope.showBanner=false;
-      }
-      // no logged user, redirect to /login
-     //   if ( next.templateUrl === ".html") {
-      //  } else {
-      //    $location.path("/login");
-       // }
-    });
-})
-.factory('MessageService', ['$rootScope', function($rootScope) {
-    $rootScope.messages = [];
+		} ]).run(
+		function($rootScope, $location) {
+			$rootScope.$on("$routeChangeStart", function(event, next, current) {
+				
+				// no logged user, redirect to /login
+				var destino=next.originalPath!==undefined?next.originalPath:current.$$route.originalPath;
+				if(destino !== undefined){
+					if (localStorage.isLogin !== undefined && localStorage.isLogin.length != 0) {
+						$rootScope.isLogin = true;
+						if (destino === '/login'
+								|| destino === '/about'
+								|| destino === '/contact'
+								|| destino === '/inicio') {
+							$location.path('/mapa');
+						}
+					} else {
+						if (destino === '/inicio') {
+							$rootScope.showBanner = true;
+						} else {
+							$rootScope.showBanner = false;
+						}
+						$rootScope.isLogin = false;
+						if (destino !== '/login'
+								&& destino !== '/about'
+								&& destino !== '/contact'
+								&& destino !== '/inicio') {
+							$location.path('/login');
+						}
+						
+					}
+				}
+			});
+		}).factory('MessageService', [ '$rootScope', function($rootScope) {
+	$rootScope.messages = [];
 
-    var MessageService = function() {
-        this.setMessages = function(messages) {
-            console.log(messages);
-            $rootScope.messages = messages;
-            $rootScope.showMessage = true;
-        };
+	var MessageService = function() {
+		this.setMessages = function(messages) {
+			console.log(messages);
+			$rootScope.messages = messages;
+			$rootScope.showMessage = true;
+		};
 
-        this.hasMessages = function() {
-            return $rootScope.messages && $rootScope.messages.length > 0;
-        }
+		this.hasMessages = function() {
+			return $rootScope.messages && $rootScope.messages.length > 0;
+		}
 
-        this.clearMessages = function() {
-            $rootScope.messages = [];
-            $rootScope.showMessage=false;
-        }
-    };
+		this.clearMessages = function() {
+			$rootScope.messages = [];
+			$rootScope.showMessage = false;
+		}
+	};
 
-    return new MessageService();
-}]).controller('appctrl', function($scope,$location,MessageService) {
+	return new MessageService();
+} ]).controller('appctrl', function($scope, $location, MessageService,UsuarioService) {
 
-			var img = 'http://localhost/webapp/img/';
-				$scope.setInterval = 5000;
-				$scope.slides = [
+	var img = 'http://localhost/webapp/img/';
+	$scope.setInterval = 5000;
+	$scope.slides = [
 
-				{
-					title : 'Imagen 1',
-					image : img + '2.jpg'
+	{
+		title : 'Imagen 1',
+		image : img + '2.jpg'
 
-				},
-				{
-					title : 'Imagen 2',
-					image : img + '4.jpg'
-			}];
-			$scope.borrarMensaje=function(){
-				MessageService.clearMessages();
-			}
-			
+	}, {
+		title : 'Imagen 2',
+		image : img + '4.jpg'
+	} ];
+	$scope.borrarMensaje = function() {
+		MessageService.clearMessages();
+	}
+	
+	
+	$scope.logout = function (){
+		var usuario="email="+localStorage.isLogin;
+		UsuarioService.logout(usuario, function(response){
+			localStorage.isLogin='';
+			$scope.login={};
+			$location.path("inicio");
+			$rootScope.showBanner = true;
+		},function (){
+			$scope.desplegarError();
+		});
+	};
+	
+	$scope.desplegarError = function() {
+		MessageService.setMessages("Error al presentar la operación.");
+	};
+
 });
